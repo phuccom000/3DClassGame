b736669899d42ec5a429ea961804f3911d259c37
phuccom000 <gdtitans6@gmail.com>
2024-09-10 19:23:46 +0700

    first chunk wip

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -4,36 +4,57 @@ using UnityEngine;
 
 public class Chunk : MonoBehaviour
 {
-    public MeshRenderer meshRenderer;
-    public MeshFilter meshFilter;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        int vertexIndex = 0;
-        List<Vector3> vertices = new List<Vector3>();
-        List<int> triangles = new List<int>();
+	public MeshRenderer meshRenderer;
+	public MeshFilter meshFilter;
 
-        for (int i = 0; i < 6; i++)
-        {
-            int triangleIndex = VoxelData.voxelTris[0,i];
-            vertices.Add(VoxelData.voxelVerts[triangleIndex]);
-            triangles.Add(vertexIndex);
+	int vertexIndex = 0;
+	List<Vector3> vertices = new List<Vector3>();
+	List<int> triangles = new List<int>();
+	List<Vector2> uvs = new List<Vector2>();
+	void Start()
+	{
+		for (int y = 0; y < VoxelData.ChunkHeigth; y++)
+		{
+			for (int x = 0; x < VoxelData.ChunkWidth; x++)
+			{
+				for (int z = 0; z < VoxelData.ChunkWidth; z++)
+				{
+					AddVoxelDataToChunk(new Vector3(x, y, z));
+				}
+			}
+		}
+		CreateMesh();
+	}
 
-            vertexIndex++;
-        }
-        Mesh mesh = new Mesh();
-        mesh.vertices = vertices.ToArray();
-        mesh.triangles = triangles.ToArray();
+	void AddVoxelDataToChunk(Vector3 pos)
+	{
+		for (int p = 0; p < 6; p++)
+		{
+			for (int i = 0; i < 6; i++)
+			{
 
-        mesh.RecalculateNormals();
+				int triangleIndex = VoxelData.voxelTris[p, i];
+				vertices.Add(VoxelData.voxelVerts[triangleIndex] + pos);
+				triangles.Add(vertexIndex);
 
-        meshFilter.mesh = mesh;
-    }
+				uvs.Add(VoxelData.voxelUvs[i]);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+				vertexIndex++;
+			}
+		}
+	}
+
+	void CreateMesh()
+	{
+		Mesh mesh = new Mesh();
+		mesh.vertices = vertices.ToArray();
+		mesh.triangles = triangles.ToArray();
+		mesh.uv = uvs.ToArray();
+
+		mesh.RecalculateNormals();
+
+		meshFilter.mesh = mesh;
+
+	}
 }
--- Assets/Scripts/VoxelData.cs
+++ Assets/Scripts/VoxelData.cs
@@ -2,22 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VoxelData : MonoBehaviour
+public static class VoxelData
 {
-    public static readonly Vector3[] voxelVerts = new Vector3[8]
-    {
-        new Vector3(0.0f, 0.0f, 0.0f),
-        new Vector3(1.0f, 0.0f, 0.0f),
-        new Vector3(1.0f, 1.0f, 0.0f),
-        new Vector3(0.0f, 1.0f, 0.0f),
-        new Vector3(0.0f, 0.0f, 1.0f),
-        new Vector3(1.0f, 0.0f, 1.0f),
-        new Vector3(1.0f, 1.0f, 1.0f),
-        new Vector3(0.0f, 1.0f, 1.0f),
-    };
 
-    public static readonly int[,] voxelTris = new int[1, 6]
-    {
-        {3, 7, 2, 2, 7, 6} // Top face
-    };
+	public static readonly int ChunkWidth = 5;
+	public static readonly int ChunkHeigth = 5;
+
+	public static readonly Vector3[] voxelVerts = new Vector3[8] {
+
+		new Vector3(0.0f, 0.0f, 0.0f),
+		new Vector3(1.0f, 0.0f, 0.0f),
+		new Vector3(1.0f, 1.0f, 0.0f),
+		new Vector3(0.0f, 1.0f, 0.0f),
+		new Vector3(0.0f, 0.0f, 1.0f),
+		new Vector3(1.0f, 0.0f, 1.0f),
+		new Vector3(1.0f, 1.0f, 1.0f),
+		new Vector3(0.0f, 1.0f, 1.0f),
+
+	};
+
+	public static readonly int[,] voxelTris = new int[6, 6] {
+
+		{0, 3, 1, 1, 3, 2}, // Back Face
+		{5, 6, 4, 4, 6, 7}, // Front Face
+		{3, 7, 2, 2, 7, 6}, // Top Face
+		{1, 5, 0, 0, 5, 4}, // Bottom Face
+		{4, 7, 0, 0, 7, 3}, // Left Face
+		{1, 2, 5, 5, 2, 6} // Right Face
+
+	};
+
+	public static readonly Vector2[] voxelUvs = new Vector2[6] {
+
+		new Vector2 (0.0f, 0.0f),
+		new Vector2 (0.0f, 1.0f),
+		new Vector2 (1.0f, 0.0f),
+		new Vector2 (1.0f, 0.0f),
+		new Vector2 (0.0f, 1.0f),
+		new Vector2 (1.0f, 1.0f)
+
+	};
+
+
 }
